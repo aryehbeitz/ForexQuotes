a7ea6bbf2dd030cab4e001555e3fefc1424d2bd7
leabarzesky <leabarzesky@gmail.com>
2017-09-02 12:29:06 +0300

    update changes

--- client/app/pages/home/home.controller.js
+++ client/app/pages/home/home.controller.js
@@ -1,10 +1,16 @@
 export default /*@ngInject*/ class HomeController {
   constructor($http) {
     this.http = $http;
+    this.base_url = 'http://www.google.com/finance/historical';
     $http.get('https://forex.1forge.com/1.0.2/symbols?api_key=LPwpGdsElf2FzN9rdIntrkFZKXk5Hals')
     .then(result => {
       this.options = result.data; 
       console.log(result);
+    })
+    $http.get('http://localhost:3005/aapl')
+    .then(result => {
+      console.log(result);
+      this.g = result.data.json();
     })
     //var me=this;
     // this.options = ['a','b'];
